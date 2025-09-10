9029a94bddbc90f4f027ceab70e2a0c0e9060d75
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2025-09-10 19:33:16 -0300

    final

--- main.js
+++ main.js
@@ -65,19 +65,7 @@ function diaEventoAhora () {
 
 
 
-  boton.addEventListener("click", () => {
-
-    Swal.fire({
-      title: '<strong>VER LA UBICACION<u></u></strong>',
-      icon: 'success',
-      html:
-        'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.9402059612808!2d-60.7524!3d-32.926178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b65296cc426771%3A0x5a60e1db9d5b092e!2sAIRES%20Sal%C3%B3n!5e0!3m2!1ses-419!2sar!4v1757453771642!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
-        
-        
-    })
-  });
-   
+ 
    
   function exito(){
     Swal.fire({
@@ -85,7 +73,7 @@ function diaEventoAhora () {
         icon: 'success',
         html:
           'INGRESÁ' +
-          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3289.577080336622!2d-58.64133612489268!3d-34.46288205007295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca58f16d59351%3A0x5c22a695fe3d1e1f!2sSmile%20sal%C3%B3n%20de%20fiestas!5e0!3m2!1ses-419!2sar!4v1722641278724!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3348.9402059612808!2d-60.7524!3d-32.926178!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b65296cc426771%3A0x5a60e1db9d5b092e!2sAIRES%20Sal%C3%B3n!5e0!3m2!1ses!2sar!4v1757542515154!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
           'Podes ampliar el mapa',
           
           
